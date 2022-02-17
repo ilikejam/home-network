03971bf402186315fb85ab5c02c6ae4732347d73
David Stark <dave@davidstark.name>
2022-02-17 17:44:32 +0000

    Lint

--- pi/display/app/display.py
+++ pi/display/app/display.py
@@ -25,60 +25,63 @@
 # Adafruit Blinka to support CircuitPython libraries. CircuitPython does
 # not support PIL/pillow (python imaging library)!
 
+import subprocess
 import sys
 import time
-import subprocess
 
-from board import SCL, SDA
-import busio
-from PIL import Image, ImageDraw, ImageFont
 import adafruit_ssd1306
+import busio
+from board import SCL, SDA
+from PIL import Image, ImageDraw, ImageFont
+
 
 class Display:
     def __init__(self):
         # Create the I2C interface.
         i2c = busio.I2C(SCL, SDA)
-        
+
         # Create the SSD1306 OLED class.
         # The first two parameters are the pixel width and pixel height.  Change these
         # to the right size for your display!
         disp = adafruit_ssd1306.SSD1306_I2C(128, 32, i2c)
-        
+
         # Clear display.
         disp.fill(0)
         disp.show()
         self.disp = disp
-    
+
         # Create blank image for drawing.
         # Make sure to create image with mode '1' for 1-bit color.
         self.width = disp.width
         self.height = disp.height
-        self.image = Image.new('1', (self.width, self.height))
-    
+        self.image = Image.new("1", (self.width, self.height))
+
         # Get drawing object to draw on image.
         self.draw = ImageDraw.Draw(self.image)
-    
+
         # Draw some shapes.
         # First define some constants to allow easy resizing of shapes.
         self.padding = 0
-    
-        self.back=0
-        self.fore=127
+
+        self.back = 0
+        self.fore = 127
 
     def display(self, message):
         print("Got message: " + message)
         sys.stdout.flush()
-        fontpath = '/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf'
+        fontpath = "/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf"
         fontsize = 28
         font = ImageFont.truetype(fontpath, fontsize)
-        while (font.getsize(message)[0] > 128 or font.getsize(message)[1] > 32) and fontsize > 9:
+        while (
+            font.getsize(message)[0] > 128 or font.getsize(message)[1] > 32
+        ) and fontsize > 9:
             fontsize -= 1
             font = ImageFont.truetype(fontpath, fontsize)
-        
+
         # Draw a black filled box to clear the image.
         self.draw.rectangle((0, 0, self.width, self.height), outline=0, fill=self.back)
-        self.draw.text((0, self.padding+0), message, font=font, fill=self.fore)
-        
+        self.draw.text((0, self.padding + 0), message, font=font, fill=self.fore)
+
         # Display image.
         self.disp.image(self.image)
         self.disp.show()
--- pi/display/app/form.py
+++ pi/display/app/form.py
@@ -2,6 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import Length
 
+
 class DisplayForm(FlaskForm):
-    message = StringField('Message', [Length(min=1, max=25)])
-    submit = SubmitField('Display')
+    message = StringField("Message", [Length(min=1, max=25)])
+    submit = SubmitField("Display")
